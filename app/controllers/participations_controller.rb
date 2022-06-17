b5a87f373a0352edc4599bf7978926d1bfe8db31
Manon Schnetzler <bonsoir@mailo.com>
2022-06-18 00:54:43 +0200

    modal ok

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -6,7 +6,7 @@ class ParticipationsController < ApplicationController
       something_went_wrong unless conversation.save
       current_user_p = Participation.new(conversation: conversation, user: current_user)
       other_user_p = Participation.new(conversation: conversation,
-                                       user: User.find(params[:participation][:user_id]))
+                                       user: User.find_by(id: params[:participation][:user_id]))
       something_went_wrong unless current_user_p.save && other_user_p.save
     end
     authorize conversation
@@ -21,7 +21,8 @@ class ParticipationsController < ApplicationController
 
   def find_conversation
     current_user_conversation = Conversation.includes(:participations).where("participations.user" => current_user)
-    other_user_conversation = Conversation.includes(:participations).where("participations.user" => params[:participation][:user_id])
+    other_user_conversation = Conversation.includes(:participations)
+                                          .where("participations.user" => params[:participation][:user_id])
     (current_user_conversation & other_user_conversation).first
   end
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -3,4 +3,5 @@ class Participation < ApplicationRecord
   belongs_to :conversation
 
   validates_uniqueness_of :user_id, scope: :conversation_id
+  validates :user, presence: true
 end
