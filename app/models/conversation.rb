78ab4b25b16a208151620bd84e5c635e09243075
Manon Schnetzler <bonsoir@mailo.com>
2022-06-21 12:36:41 +0200

    show ok

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,7 @@
 class Conversation < ApplicationRecord
-  has_many :participations
+  has_many :participations, dependent: :destroy
   has_many :users, through: :participations
-  has_many :milts
+  has_many :milts, dependent: :destroy
 
   attr_accessor :other_user
 
